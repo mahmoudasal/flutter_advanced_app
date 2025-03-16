8cf1ea496350dfc90c60e03dbc01354946489e5c
MahmoudAsal <Mahmoudasal656@gmail.com>
2025-03-16 16:41:59 +0200

    - just a fake push to origin development

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,6 @@ import 'package:flutter_advanced_app/core/routing/app_router.dart';
 import 'package:flutter_advanced_app/doc_app.dart';
 
 void main() {
+  
   runApp(DocApp(appRouter: AppRouter()));
 }
